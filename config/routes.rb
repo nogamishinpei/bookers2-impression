0dcddd7133e338f7f36641822c12dcfa19a938b9
nogamishinpei <nogamichiru@gmail.com>
2021-07-18 02:24:27 +0000

    [add]html記述完了

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   get 'users/show'
   devise_for :users
   root to: 'homes#top'
-  resources :books, only: [:new, :create, :index, :show, :destroy]
+  
+  
+  resources :books
+  resources :users, only: [:new, :create, :index, :show, :destroy]
+
 end
+
