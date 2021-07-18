9c519704fe28de591392edd5bbea2128bad71f37
nogamishinpei <nogamichiru@gmail.com>
2021-07-18 06:47:15 +0000

    [add]4-10about追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,24 +5,47 @@ class BooksController < ApplicationController
   end
 
   def create
-     @book = book.new(book_params)
+    @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to books_path
+    if @book.save
+      flash[:notice]="You have creatad book successfully."
+      redirect_to book_path(@book)
+    else
+      @user = current_user
+      @books = Book.all
+      render :index
+    end
   end
 
-  def index
+   def index
+    @user = current_user
+    @book = Book.new
     @books = Book.all
-    @book = Book
-  end
+   end
 
   def show
+    @newbook = Book.new
+    @book = Book.find(params[:id])
+    @user = @book.user
   end
 
+  def update
+    @book = Book.find(params[:id])
+    @book.user_id = current_user.id
+    if @book.update(book_params)
+      flash[:notice]="Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+  end
+  
   def destroy
     @book = Book.find(params[:id])
-    @book.destroy
-    redirect_to book_path
+    if @book.destroy
+      flash[:notice]="Book was successfully destroyed."
+      redirect_to books_path
+    end
   end
   
   
