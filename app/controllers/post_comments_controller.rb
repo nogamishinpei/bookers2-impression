371e79c8d0120488a17ac906f00160f6db8e22ef
nogamishinpei <nogamichiru@gmail.com>
2021-08-07 06:58:56 +0000

    [add]非同期通信いいねとコメント機能

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -3,22 +3,23 @@ class PostCommentsController < ApplicationController
    before_action :baria_user, only: [:destroy]
   
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     comment = current_user.post_comments.new(post_comment_params)
-    comment.book_id = book.id
+    comment.book_id = @book.id
     comment.save
-    redirect_to book_path(book)
   end
   
   def destroy
-    PostComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    redirect_to book_path(params[:book_id])
+    @book = Book.find(params[:book_id])
+    comment = PostComment.find(params[:id])
+    comment.destroy
+    
   end
 
  private
 
   def post_comment_params
-    params.require(:post_comment).permit(:comment)
+    params.require(:post_comment). permit(:comment, :user_id, :book_id)
   end
   
   def baria_user
@@ -29,4 +30,3 @@ class PostCommentsController < ApplicationController
   end
   
 end
-
