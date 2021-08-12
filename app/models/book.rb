167139b450a4b749bfd7577320627d6a4dc2406a
nogamishinpei <nogamichiru@gmail.com>
2021-08-12 05:31:12 +0000

    [add]コード整理

--- app/models/book.rb
+++ app/models/book.rb
@@ -22,15 +22,15 @@ def self.search(search, word)
 
   if search == "forward_match"
     @book = Book.where("title LIKE?","#{word}%")
-    elsif search == "backward_match"
-      @book = Book.where("title LIKE?","%#{word}")
-      elsif search =="perfect_match"
-        @book = Book.where(title: word)
-        elsif search == "partial_match"
-          @book = Book.where("title LIKE?","%#{word}%")
-        else
-          @book = Book.all
-        end
+  elsif search == "backward_match"
+    @book = Book.where("title LIKE?","%#{word}")
+  elsif search =="perfect_match"
+    @book = Book.where(title: word)
+  elsif search == "partial_match"
+    @book = Book.where("title LIKE?","%#{word}%")
+  else
+    @book = Book.all
+  end
 end
 #閲覧数カウント機能
 def change
